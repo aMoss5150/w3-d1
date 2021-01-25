b8bea66450aef7a14fe9a5671fb1f26f3d89f8f8
Walter <andrewmossaudio@gmail.com>
2021-01-25 16:12:47 -0600

    solved third problem -am

--- 02-feed-pet.js
+++ 02-feed-pet.js
@@ -5,21 +5,27 @@ function that, when invoked with a food, will return the pet's name and a list
 of foods that you have fed that pet.
 
 */
-
+debugger
 function feedPet(name) {
   const foods = [];
+  debugger
   return (food) => {
-    return "Fed " + name + " " + foods.push(food) + ".";
+    debugger
+    foods.push(food)
+    return "Fed " + name + " " + foods + ".";
   }
 }
-
+debugger
 const feedHydra = feedPet('Hydra');
-
+debugger
 console.log(feedHydra('bones')); // Fed Hyrda bones.
+debugger
 console.log(feedHydra('Hercules')); // Fed Hyrda bones, Hercules.
-
+debugger
 const feedHippogriff = feedPet('Hippogriff');
-
+debugger
 console.log(feedHippogriff('worms')); // Fed Hyrda worms.
+debugger
 console.log(feedHippogriff('crickets')); // Fed Hyrda worms, crickets.
+debugger
 console.log(feedHippogriff('chicken')); // Fed Hyrda worms, crickets, chicken.
